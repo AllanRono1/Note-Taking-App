deb530602f8485f0e49da1dc0bc5908ae3633d29
AllanRono1 <allanrono18@gmail.com>
2026-03-08 17:43:19 +0300

    Added functionality to the 'Add' button that renders out the inputs below

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+let myLeads = []
+
 const saveEl = document.getElementById("save-el")
 const inputEl = document.getElementById("input-el")
+const inputList = document.getElementById("list-items")
 
+saveEl.addEventListener("click", function(){
+    myLeads.push(inputEl.value);
+    inputEl.value= "";
+    render()
+
+})
+
+function render() {
+    let list = ""
+    for(let i = 0; i < myLeads.length; i++) {
+        list += `<li><a href="#">${myLeads[i]}</a></li>`
+    }
+    inputList.innerHTML = list
+}
