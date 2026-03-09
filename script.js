f4a7cbd177eb725e6b76c4eaff392471613d5df4
AllanRono1 <allanrono18@gmail.com>
2026-03-09 14:42:24 +0300

    Style rendered notes and add Edit, Delete and Done buttons

--- script.js
+++ script.js
@@ -3,10 +3,15 @@ let myLeads = []
 const saveEl = document.getElementById("save-el")
 const inputEl = document.getElementById("input-el")
 const inputList = document.getElementById("list-items")
+const storageValues = JSON.parse(localStorage.getItem("myLeads"))
+if(storageValues) {
+    render()
+}
 
 saveEl.addEventListener("click", function(){
     myLeads.push(inputEl.value);
     inputEl.value= "";
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
     render()
 
 })
@@ -16,7 +21,16 @@ function render() {
     for(let i = 0; i < myLeads.length; i++) {
         list += `
         <li>
-        <a href="#">${myLeads[i]}</a>
+        <div class="index-container">
+        <div class="link-anchor">
+            <a href="#">${myLeads[i]}</a>
+        </div>
+        <div>
+            <button class="edit" title="delete">📝</button>
+            <button class="edit clear" title="delete">❌</button>
+            <button class="edit delete" title="done">✅</button>
+        </div>
+        </div>
         </li>
         `
     }
